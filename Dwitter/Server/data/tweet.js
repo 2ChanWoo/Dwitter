35323e3b6bdc78da7608a1fcc6c39893cf81d027
2chanwoo <navillela.808@gmail.com>
2023-01-01 03:56:58 +0900

    사용자 인증 적용.

--- Dwitter/Server/data/tweet.js
+++ Dwitter/Server/data/tweet.js
@@ -1,31 +1,51 @@
+import * as userRepository from './auth_repository.js';
+
 let tweets = [
     {
       id: '1',
       text: '드림코더분들 화이팅!',
-      createdAt: Date.now().toString(),
-      name: 'Bob',
-      username: 'bob',
-      url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
+      createdAt: new Date().toString(),
+      userId: '1'
     },
     {
       id: '2',
       text: '안뇽!',
       createdAt: Date.now().toString(),
-      name: 'Ellie',
-      username: 'ellie',
+      userId: '2'
     },
   ];
   
   export async function getAll() {
-    return tweets;
+    return Promise.all(
+      tweets.map(async (tweet) => {
+        const {username, name, url} = await userRepository.findById(tweet.userId);
+        return {...tweet, username, name, url};
+      })
+    );
   }
   
+  //! promise 헷갈리는데..?! promise.then 을 return 으로 넘겨도..
   export async function getAllByUsername(username) {
-    return tweets.filter((tweet) => tweet.username === username);
+    return getAll().then((tweets) => 
+      tweets.filter((tweet) => tweet.username === username)
+    );
   }
   
+  /// '트윗' 아이디로부터 객체 찾기.
   export async function getById(id) {
-    return tweets.find((tweet) => tweet.id === id);
+    const found = tweets.find((tweet) => tweet.id === id);
+    if(!found) return null;
+    const {username, name, url} = userRepository.findById((user) => user.id === found.userId);
+    return {...found, username, name, url};
+    //* 아래는 내가 작성한 로직. getAll은 비용이 크기 때문에 위 드림코딩 로직이 더 좋아보임.
+    //* 생각해보니까.. 트윗이 많으면, getAll 로직 비효율적인거 아니야..?!
+    //* 는... 유저정보 바꿔도 그대로인것 보다는 이게 더 나은거 같긴 하지만 포린키 넣는게 베스트인듯?!
+    //! 나중에 질문을 해 보자! 디비 구성 파트까지 보구
+    /**
+    return getAll().then((tweets) => 
+      tweets.find((tweet) => tweet.id === id)
+    );
+    */
   }
   
   export async function create(text, name, username) {
@@ -33,11 +53,10 @@ let tweets = [
       id: Date.now().toString(),
       text,
       createdAt: new Date(),
-      name,
-      username,
+      userid,
     };
     tweets = [tweet, ...tweets];
-    return tweet;
+    return getById(tweet.id);
   }
   
   export async function update(id, text) {
@@ -45,7 +64,7 @@ let tweets = [
     if (tweet) {
       tweet.text = text;
     }
-    return tweet;
+    return getById(tweet.id);
   }
   
   export async function remove(id) {
